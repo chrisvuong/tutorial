7e8abd8feb610f6eb3861184d6727d2238694024
Chris Vuong <chris.v@payadvantage.com>
2023-08-12 12:58:56 +1000

    Tuan 1

---
+++ tuan 1/chrisfile.py
@@ -0,0 +1,3 @@
+# here is chris file
+name = input("Nhap ten: ")
+print("Ten la ", name)
---
+++ tuan 1/so_sanh.py
@@ -0,0 +1,9 @@
+a = int(input("Nhap ao so dau tien: "))
+b = int(input("Nhap ao so dau thu hai: "))
+if a > b:
+    print("a lon hon b")
+elif a < b:
+    print("a nho hon b")
+else:
+    print ("a bang b")
+
