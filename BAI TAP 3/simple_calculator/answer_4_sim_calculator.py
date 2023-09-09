41182b020510e431b2c8f0f6ac0e60b699ddb6fb
Leovisvuong <vbtst001@gmail.com>
2023-09-09 13:04:53 +0700

    fix_bug_4_answer_4_sim_calculator

--- BAI TAP 3/simple_calculator/answer_4_sim_calculator.py
+++ BAI TAP 3/simple_calculator/answer_4_sim_calculator.py
@@ -1,5 +1,8 @@
+dk = [chr(a) for a in range(ord('1'),ord('9')+1)]
 k='e'
 while k!='q':
+    k='e'
+    x=y=0
     choice = int(input("""Select operation.
     1.Add
     2.Subtract
@@ -7,8 +10,12 @@ while k!='q':
     4.Divide
     5.Convert
     Enter choice(1/2/3/4/5): """))
-    x = int(input('add first number: '))
-    y = int(input('add second number: '))
+    while x not in dk:
+        x = input('add first number: ')
+    while y not in dk:
+        y = input('add second number: ')
+    x=int(x)
+    y=int(y)
     if choice==1:
         kq=x+y
     elif choice==2:
@@ -17,7 +24,7 @@ while k!='q':
         kq=x/y
     elif choice==4:
         kq=x*y
-    else:
-        quit()
     print(kq)
     k=str(input('do you want to continue?(press"q" to quit or "c" to continue): '))
+    while k!='q' and k!='c':
+        k=str(input('press"q" to quit or "c" to continue: '))
